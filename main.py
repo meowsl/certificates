440ba4b02ce7dd32cfad1ab1ba693739ba50b334
Valter Frank <glup0st1@mail.ru>
2024-02-13 20:04:02 +0300

    feat: progress bar & add threads

--- main.py
+++ main.py
@@ -6,23 +6,23 @@ from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 from PIL import ImageFont
 import openpyxl
+import time
+import threading
+from queue import Queue
+from alive_progress import alive_bar
+from progress.bar import IncrementalBar, Bar
 
 def create_cert(lastname, firstname, midname, id_text):
 
-    old_pdf = PdfReader('inputs/cert_without_sign.pdf')
-    sz = old_pdf.pages[0].mediabox
-
     packet = io.BytesIO()
 
-    font_name = "fonts/static/OpenSans-Regular.ttf"
-    font = ImageFont.truetype(font_name, 18)
     name = f'{lastname} {firstname} {midname}'
     name_width, name_height = font.getmask(name).size
 
     can = canvas.Canvas(packet, pagesize=letter)
 
-    text_x = (sz.width - name_width) / 2
-    text_y = (sz.height - name_height) / 2
+    text_x = (size.width - name_width) / 2
+    text_y = (size.height - name_height) / 2
     can.setFont('OpenSansItalic', 12)
     can.drawString(200, int(text_y) - 210, id_text)
 
@@ -33,16 +33,16 @@ def create_cert(lastname, firstname, midname, id_text):
     can.save()
 
     new_pdf = PdfReader(packet)
-    existing_pdf = PdfWriter(open("inputs/cert_without_sign.pdf", "rb"))
     output = PdfWriter()
 
     page = old_pdf.pages[0]
     page.merge_page(new_pdf.pages[0])
     output.add_page(page)
-    output_stream = open(f"outputs/{lastname}_{firstname[0]}._{midname[0]}.pdf", "wb")
-    output.write(output_stream)
-    output_stream.close()
-    print(f'{lastname}_{firstname[0]}._{midname[0]}.pdf was created!')
+
+    with open(f"outputs/{lastname}_{firstname[0]}._{midname[0]}.pdf", "wb") as output_stream:
+      output.write(output_stream)
+
+    bar.next()
 
 def extract_elements(row):
     elements = []
@@ -51,14 +51,59 @@ def extract_elements(row):
     return elements
 
 def extract_students():
+    global bar, start
     wb = openpyxl.load_workbook('inputs/students.xlsx')
     sheet = wb['Лист1']
+
+    # Очередь для хранения данных о студентах
+    queue = Queue()
+    start = time.time()
+
+    # Инициализация progress bar
+    bar = Bar('Progress: ', max=sheet.max_row, fill='*', suffix='%(percent)d%%')
+
+    # Функция для потоков
+    def worker():
+        while True:
+            # Получить данные о студенте из очереди
+            elements = queue.get()
+            if elements is None:
+                break
+            create_cert(elements[1], elements[2], elements[3], elements[0])
+
+    # Запуск потоков
+    num_threads = 10  # Количество потоков
+    threads = []
+    for _ in range(num_threads):
+        thread = threading.Thread(target=worker)
+        thread.start()
+        threads.append(thread)
+        bar.update()
+
+    # Добавление данных о студентах в очередь
     for row in sheet.iter_rows():
         elements = extract_elements(row)
-        create_cert(elements[1], elements[2], elements[3], elements[0])
+        queue.put(elements)
+
+    # Остановка потоков
+    for _ in range(num_threads):
+        queue.put(None)
+
+    for thread in threads:
+        thread.join()
+
+    bar.finish()
 
 if __name__ == "__main__":
     # Register font to support Cyrillic characters
-    pdfmetrics.registerFont(TTFont('OpenSans', 'fonts/static/OpenSans-Regular.ttf'))
-    pdfmetrics.registerFont(TTFont('OpenSansItalic', 'fonts/static/OpenSans_SemiCondensed-Italic.ttf'))
+    pdfmetrics.registerFont(TTFont('OpenSans', 'fonts/OpenSans-Regular.ttf'))
+    pdfmetrics.registerFont(TTFont('OpenSansItalic', 'fonts/OpenSans_SemiCondensed-Italic.ttf'))
+
+    old_pdf = PdfReader('inputs/cert.pdf')
+    size = old_pdf.pages[0].mediabox
+    font = ImageFont.truetype("fonts/OpenSans-Regular.ttf", 18)
+
+    print('Start')
     extract_students()
+    end = time.time()
+    print(f'Выполнилось за {end - start}')
