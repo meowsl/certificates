ef191f2c3fe701560e510392ecf22f45cd484e94
meowsl <vlafrank@ya.ru>
2024-02-14 13:14:41 +0300

    fix: threads

--- main.py
+++ main.py
@@ -1,109 +1,69 @@
-from PyPDF2 import PdfReader, PdfWriter
 import io
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 from PIL import ImageFont
-import openpyxl
+from PyPDF2 import PdfReader, PdfWriter
 import time
-import threading
-from queue import Queue
-from alive_progress import alive_bar
+import openpyxl
+from concurrent.futures import ThreadPoolExecutor
 from progress.bar import IncrementalBar, Bar
 
 def create_cert(lastname, firstname, midname, id_text):
-
     packet = io.BytesIO()
-
     name = f'{lastname} {firstname} {midname}'
     name_width, name_height = font.getmask(name).size
-
     can = canvas.Canvas(packet, pagesize=letter)
 
     text_x = (size.width - name_width) / 2
     text_y = (size.height - name_height) / 2
-    can.setFont('OpenSansItalic', 12)
+
+    can.setFont("OpenSansItalic", 12)
     can.drawString(200, int(text_y) - 210, id_text)
 
-    can.setFont('OpenSans', 18)
-    can.drawString(int(text_x), int(text_y) - 25 , name)
+    can.setFont("OpenSans", 18)
+    can.drawString(int(text_x), int(text_y) - 25, name)
 
     packet.seek(0)
     can.save()
 
     new_pdf = PdfReader(packet)
     output = PdfWriter()
-
     page = old_pdf.pages[0]
     page.merge_page(new_pdf.pages[0])
     output.add_page(page)
 
-    with open(f"outputs/{lastname}_{firstname[0]}._{midname[0]}.pdf", "wb") as output_stream:
-      output.write(output_stream)
+    with open(f"outputs/{lastname}_{firstname[0]}_{midname[0]}.pdf", "wb") as output_stream:
+        output.write(output_stream)
 
     bar.next()
 
-def extract_elements(row):
-    elements = []
-    for cell in row:
-        elements.append(str(cell.value))
-    return elements
+def process_row(row):
+    elems = [str(cell.value) for cell in row]
+    create_cert(elems[1], elems[2], elems[3], elems[0])
 
 def extract_students():
-    global bar, start
-    wb = openpyxl.load_workbook('inputs/students.xlsx')
-    sheet = wb['Лист1']
+    global old_pdf, font, size
 
-    # Очередь для хранения данных о студентах
-    queue = Queue()
-    start = time.time()
-
-    # Инициализация progress bar
-    bar = Bar('Progress: ', max=sheet.max_row, fill='*', suffix='%(percent)d%%')
-
-    # Функция для потоков
-    def worker():
-        while True:
-            # Получить данные о студенте из очереди
-            elements = queue.get()
-            if elements is None:
-                break
-            create_cert(elements[1], elements[2], elements[3], elements[0])
-
-    # Запуск потоков
-    num_threads = 10  # Количество потоков
-    threads = []
-    for _ in range(num_threads):
-        thread = threading.Thread(target=worker)
-        thread.start()
-        threads.append(thread)
-        bar.update()
-
-    # Добавление данных о студентах в очередь
-    for row in sheet.iter_rows():
-        elements = extract_elements(row)
-        queue.put(elements)
-
-    # Остановка потоков
-    for _ in range(num_threads):
-        queue.put(None)
-
-    for thread in threads:
-        thread.join()
-
-    bar.finish()
-
-if __name__ == "__main__":
-    # Register font to support Cyrillic characters
     pdfmetrics.registerFont(TTFont('OpenSans', 'fonts/OpenSans-Regular.ttf'))
     pdfmetrics.registerFont(TTFont('OpenSansItalic', 'fonts/OpenSans_SemiCondensed-Italic.ttf'))
-
     old_pdf = PdfReader('inputs/cert.pdf')
-    size = old_pdf.pages[0].mediabox
+    old_pdf_page = old_pdf.pages[0]
+    size = old_pdf_page.mediabox
     font = ImageFont.truetype("fonts/OpenSans-Regular.ttf", 18)
 
-    print('Start')
+    with ThreadPoolExecutor() as executor:
+        for row in sheet.iter_rows():
+            executor.submit(process_row, row)
+
+if __name__ == "__main__":
+    start = time.time()
+
+    wb = openpyxl.load_workbook('inputs/students.xlsx')
+    sheet = wb['Лист1']
+    bar = Bar('Progress: ', max=sheet.max_row, fill='*', suffix='%(percent)d%%')
+    bar.update()
     extract_students()
-    end = time.time()
-    print(f'Выполнилось за {end - start}')
+
+    print(f'Выполнилось за {time.time() - start} секунд')
